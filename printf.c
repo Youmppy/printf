ab23c440dafc930609b4ad549ebe94d6f515c2fc
Youmppy <headsch00l@gmail.com>
2023-09-19 16:27:39 -0700

    12

---
+++ printf.c
@@ -0,0 +1,91 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * format_checker - Custom printf function
+ * @c: The format specifier character.
+ * @ap: The va_list containing the arguments.
+ *
+ * This function processes the format specifier
+ * and prints the corresponding values.
+ */
+void format_checker(char c, va_list ap)
+{
+	if (c == 'c')
+	{
+		char ch = va_arg(ap, int);
+
+		write(1, &ch, 1);
+	}
+	else if (c == 's')
+	{
+		char *str = va_arg(ap, char *);
+
+		if (str != NULL)
+		{
+			while (*str)
+			{
+				write(1, str, 1);
+				str++;
+			}
+		}
+		else
+		{
+			write(1, "(null)", 6);
+		}
+	}
+	else if (c == '%')
+	{
+		write(1, "%", 1);
+	}
+	else if (c == 'd' || c == 'i')
+	{
+		int num = va_arg(ap, int);
+
+		d_i(num);
+	}
+	else if (c == 'b')
+	{
+		int num = va_arg(ap, int);
+
+		b_print(num);
+	}
+}
+
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	va_list ap;
+
+	va_start(ap, format);
+	if (!format)
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			write(1, format + i, 1);
+			i++;
+		}
+		else
+		{
+			i++;
+			if (format[i] != '\0')
+			{
+				format_checker(format[i], ap);
+				i++;
+			}
+		}
+	}
+
+	va_end(ap);
+	return (i);
+}
---
+++ task_0-2.c
@@ -0,0 +1,104 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * d_i - prints int
+ * @d: int to print
+ * Return: void
+ */
+void d_i(int d)
+{
+	int n = 0;
+	int b = d;
+	int negative = 0;
+	int digit_num = 0;
+	int s_n = 0;
+	char s[32];
+
+	for (n = 1; b / 10 != 0; n++)
+	{
+		b = b / 10;
+	}
+
+	if (d < 0)
+	{
+		d = -d;
+		negative++;
+	}
+	n++;
+	s_n = n;
+
+	n--;
+	s[n] = '\0';
+	n--;
+	while (d != 0)
+	{
+		digit_num = d % 10;
+		s[n] = '0' + digit_num;
+		d = d / 10;
+		n--;
+	}
+
+	if (negative)
+	{
+		write(1, "-", 1);
+	}
+	write(1, s, s_n);
+}
+
+/**
+ * b_print - prints binary
+ * @x: int to convert to binary and print
+ * Return: void
+ */
+void b_print(int x)
+{
+	int j = x;
+	int n = 0;
+	int s_n = 0;
+	char b[32];
+
+	for (n = 1; j / 2 != 0; n++)
+	{
+		j = j / 2;
+	}
+
+	s_n = n;
+
+	n--;
+	b[n] = '\0';
+
+	while (x != 0)
+	{
+		if (x % 2 == 0)
+		{
+			b[n] = '0';
+		}
+		else
+		{
+			b[n] = '1';
+		}
+		x = x / 2;
+		n--;
+	}
+
+	write(1, b, s_n);
+}
+
+/**
+ * str_print - prints string
+ * @s: string to print
+ * Return: void
+ */
+void str_print(const char *s)
+{
+	int m = 0;
+
+	while (s[m] != '\0')
+	{
+		m++;
+	}
+
+	write(1, s, m);
+}
