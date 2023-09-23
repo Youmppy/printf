555bc6631c89106fc498db108b09d57a88dbd8aa
Youmppy <headsch00l@gmail.com>
2023-09-22 18:17:23 -0700

    i

--- tasks0-2.c
+++ tasks0-2.c
@@ -58,3 +58,47 @@ int str_print(const char *s)
 	write(1, s, m);
 	return (i);
 }
+
+/**
+ * d_i - prints int
+ * @d: int to print
+ * Return: void
+ */
+
+void d_i(int d)
+{
+	int n = 0;
+	int b = d;
+	int negative = 0;
+	int digit_num = 0;
+	int s_n = 0;
+
+	for (n = 1; b / 10 != 0; n++)
+	{
+		b = b / 10;
+	}
+	if (d < 0)
+	{
+		d = -d;
+		negative++;
+	}
+	n++;
+	s_n = 0;
+	char s[n];
+
+	n--;
+	s[n] = '\0';
+	n--;
+	while (d != 0)
+	{
+		digit_num = d % 10;
+		s[n] = '0' + digit_num;
+		d = d / 10;
+		n--;
+	}
+	if (negative)
+	{
+		write(1, "-", 1);
+	}
+	write(1, s, s_n);
+}
